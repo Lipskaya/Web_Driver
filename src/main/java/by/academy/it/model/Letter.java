eac3b45147818f25ad9905eacb9737425396d33c
Tamara Kalachova <Lipskaya.tamara@gmail.com>
2020-02-05 22:36:55 +0300

    Business models

---
+++ src/main/java/by/academy/it/model/Letter.java
@@ -0,0 +1,31 @@
+package by.academy.it.model;
+
+public class Letter {
+  private String address;
+  private String topic;
+  private String message;
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getTopic() {
+    return topic;
+  }
+
+  public void setTopic(String topic) {
+    this.topic = topic;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+}
